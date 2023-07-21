4380d1ff5c7b561ef3e56894ba567c950d3c6055
V-Mokhun <mohunmax1408@gmail.com>
2023-07-21 10:44:12 +0200

    Added get token function

--- src/shared/api/types.ts
+++ src/shared/api/types.ts
@@ -1,3 +1,9 @@
+export interface Token {
+  access_token: string,
+  expires_in: number,
+  token_type: string
+}
+
 export enum GameStatus {
   "NONE" = "None",
   "WANT_TO_PLAY" = "Want to Play",
--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -2,5 +2,6 @@ export { cn } from "./cn";
 export { catchError, displayError, throwError } from "./error";
 export { isEmail } from "./is-email";
 export { isDefaultAvatar } from './is-default-avatar';
+export { getToken } from './token';
 
 export * from './hooks';
