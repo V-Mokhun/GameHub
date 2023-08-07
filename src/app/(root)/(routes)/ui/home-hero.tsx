19ef14b657bc987f75e24df16e3bf7c9ba163109
V-Mokhun <mohunmax1408@gmail.com>
2023-08-07 09:49:05 +0200

    Improved home hero

--- src/app/(root)/(routes)/ui/home-hero.tsx
+++ src/app/(root)/(routes)/ui/home-hero.tsx
@@ -1,26 +1,38 @@
-import { Container, Subtitle, Title } from "@shared/ui";
+import { BROWSE_ROUTE } from "@shared/consts";
+import { Container, Title, buttonVariants } from "@shared/ui";
 import Image from "next/image";
+import Link from "next/link";
 
 export const HomeHero = ({}) => {
   return (
-    <section className="relative -mt-5 md:-mt-3 flex flex-col items-center justify-center min-h-[30vh] mb-4 md:mb-6">
+    <section className="relative -mt-6 md:-mt-4 flex flex-col items-center justify-center min-h-[30vh] mb-4 md:mb-6 overflow-hidden">
       <div className="absolute left-0 right-0 h-full">
         <Image
           src={"/images/home-banner.jpg"}
           fill
-          className="aspect-video object-cover blur-sm"
+          className="aspect-video object-cover blur-[2px]"
           alt="Games banner"
+          priority
         />
       </div>
-      <Container>
-        <div className="text-white relative z-[1] text-center py-12 md:py-16">
-          <Title size="large" className="drop-shadow-lg">
+      <Container className=" relative z-[1] bg-black/20">
+        <div className="text-white text-center py-12 md:py-16">
+          <Title size="huge" className="drop-shadow-lg">
             Unlock Your Gaming Journey
           </Title>
           <Title size="small" className="drop-shadow-lg">
             Your one-stop destination for game information, library management,
             and community engagement.
           </Title>
+          <Link
+            href={BROWSE_ROUTE}
+            className={buttonVariants({
+              size: "lg",
+              className: "md:mt-6 mt-4 md:text-lg",
+            })}
+          >
+            Browse Games
+          </Link>
         </div>
       </Container>
     </section>
