626ced27eb3e0dd694ca28a2671e16f11ffcc5cf
V-Mokhun <mohunmax1408@gmail.com>
2023-09-22 15:04:51 +0200

    Added ui when game has no reviews

--- src/app/(root)/(routes)/games/[gameId]/ui/game-reviews.tsx
+++ src/app/(root)/(routes)/games/[gameId]/ui/game-reviews.tsx
@@ -1,16 +1,22 @@
 "use client";
 
+import { useAuth } from "@clerk/nextjs";
 import { ReviewsItem } from "@entities/review";
 import { ReviewSortFields, SortFieldsOrder, gamesApi } from "@shared/api";
-import { REVIEWS_ROUTE } from "@shared/consts";
-import { Icon, Link, Title } from "@shared/ui";
+import { CREATE_REVIEW_ROUTE, REVIEWS_ROUTE } from "@shared/consts";
+import { useCustomToasts } from "@shared/lib/hooks";
+import { Icon, Link, Subtitle, Title } from "@shared/ui";
 import NextLink from "next/link";
+import { useRouter } from "next/navigation";
 
 interface GameReviewsProps {
   gameId: string;
 }
 
 export const GameReviews = ({ gameId }: GameReviewsProps) => {
+  const { userId } = useAuth();
+  const { signInToast } = useCustomToasts();
+  const router = useRouter();
   const { data } = gamesApi.getReviews(
     gameId,
     {
@@ -31,17 +37,38 @@ export const GameReviews = ({ gameId }: GameReviewsProps) => {
           >
             User Reviews{" "}
             <Icon
-              className="inline-block text-inherit w-8 h-8"
+              className="inline-block text-inherit w-7 h-7 md:w-8 md:h-8"
               name="ChevronRight"
             />
           </NextLink>
         </Title>
-        <ul>
-          {data.reviews.map((review) => (
-            <ReviewsItem review={review} gameId={gameId} key={review.id} />
-          ))}
-        </ul>
-        <Link href={REVIEWS_ROUTE(gameId)} className="text-center block">See all reviews</Link>
+        {data.reviews.length > 0 ? (
+          <>
+            <ul>
+              {data.reviews.map((review) => (
+                <ReviewsItem review={review} gameId={gameId} key={review.id} />
+              ))}
+            </ul>
+            <Link href={REVIEWS_ROUTE(gameId)} className="text-center block">
+              See all reviews
+            </Link>
+          </>
+        ) : (
+          <Subtitle size="large">
+            This game has no reviews yet.{" "}
+            <button
+              type="button"
+              className="text-primary hover:text-primary-hover transition-colors"
+              onClick={() => {
+                if (!userId) signInToast();
+
+                router.push(CREATE_REVIEW_ROUTE(gameId));
+              }}
+            >
+              Be the one to write the first review! <Icon name="ChevronsRight" className="text-inherit w-6 h-6 inline-block" />
+            </button>
+          </Subtitle>
+        )}
       </div>
     )
   );
